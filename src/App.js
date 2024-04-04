028959d7a2857c482cc14ad4df65e144b1daf5f7
Youngseok Hwang <hysgold@etechsystem.co.kr>
2024-04-04 16:37:31 +0900

    App.js change

--- src/App.js
+++ src/App.js
@@ -4,30 +4,64 @@ import { OrbitControls } from '@react-three/drei';
 import Character from './model/character.jsx';
 import Building from './model/building.jsx';
 import Test from './model/test.jsx';
-import Null from './model/Null.jsx';
+// import Null from './model/Null.jsx'; // <- 이 파일은 손상되어 사용 불가능!
 
 export default function App() {
    return (
       <>
-         <Canvas
-            camera={{ position: [15, 5, 22.25], fov: 0.5 }}
-            style={{
-               backgroundColor: '#111a21',
-               width: '100%',
-               height: '25vh',
-            }}
-         >
-            <ambientLight intensity={1.25} />
-            <ambientLight intensity={-0.5} />
-            <directionalLight intensity={15.4} />
-            <Suspense fallback={null}>
-               {/* <Character position={[0.025, -0.9, 0]} /> */}
-               <Building position={[0, 0, 0]} />
-               {/* <Test position={[-1.5, -1.5, 0]} /> */}
-               {/* <Null position={[0,0,0]} /> */}
-            </Suspense>
-            <OrbitControls />
-         </Canvas>
+         <div class="d-flex">
+            
+            <Canvas
+               camera={{ position: [15, 5, 22.25], fov: 0.6 }}
+               style={{
+                  backgroundColor: '#111a21',
+                  width: '35vw',
+                  height: '100vh',
+               }}
+            >
+               <Suspense fallback={null}>
+                  <Building position={[-0.035, -0.05, 0]} />
+               </Suspense>
+               <OrbitControls />
+               <ambientLight intensity={1.25} />
+               <directionalLight intensity={3.4} />
+            </Canvas>
+
+            <Canvas
+               camera={{ position: [15, 5, 22.25], fov: 8 }}
+               style={{
+                  backgroundColor: '#1E2E3B',
+                  width: '33vw',
+                  height: '100vh',
+               }}
+            >
+               <Suspense fallback={null}>
+                  <Character position={[0.025, -0.9, 0]} />
+               </Suspense>
+               <OrbitControls />
+               <ambientLight intensity={0.75} />
+               <directionalLight intensity={2.4} />
+            </Canvas>
+            
+            <Canvas
+               camera={{ position: [15, 5, 22.25], fov: 25 }}
+               style={{
+                  backgroundColor: '#2C4354',
+                  width: '33vw',
+                  height: '100vh',
+               }}
+            >
+               <Suspense fallback={null}>
+                  <Test position={[-2.5, -2.2, 0]} />
+               </Suspense>
+               <OrbitControls />
+               <ambientLight intensity={0.15} />
+               <directionalLight intensity={2.4} />
+            </Canvas>
+
+         </div>
+         
+         
       </>
    );
 }
--- src/model/character.jsx
+++ src/model/character.jsx
@@ -24,4 +24,4 @@ export default function Model(props) {
   )
 }
 
-useGLTF.preload('/character.glb')
+useGLTF.preload('/glb/character.glb')
